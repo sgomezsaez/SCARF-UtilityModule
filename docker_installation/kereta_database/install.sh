0764329e70f7f6aed44c6b230a6643202a3b48de
ubuntu <ubuntu@SG-MT-Utility_Eval_Topologies.informatik.uni-stuttgart.de>
2016-06-06 17:57:08 +0200

    kereta app and database provisioning

---
+++ docker_installation/kereta_database/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+KERETA_DATABASE_NAME="Kereta"
+KERETA_DATABASE_USER="root"
+KERETA_DATABASE_PASSWORD="root"
+
+#Running Database
+docker run --name mysql_$KERETA_DATABASE_NAME -e MYSQL_ROOT_PASSWORD=$KERETA_DATABASE_PASSWORD -e $MYSQL_DATABASE=$KERETA_DATABASE_NAME -d mysql/mysql-server:5.5
+
+#Connect to database from an application in another container
+#docker run --name app-container-name --link my-container-name:mysql -d app-that-uses-mysql
+
+#Connect through mysql client from another container
+#docker run -it --link my-container-name:mysql_$KERETA_DATABASE_NAME --rm mysql/mysql-server:tag sh -c 'exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$KERETA_DATABASE_PASSWORD"'
