0047905fc2426f6242473490fa711f8bb00fbaf6
ubuntu <ubuntu@SG-MT-Utility_Eval_Topologies.informatik.uni-stuttgart.de>
2016-06-06 17:59:11 +0200

    minor change

--- docker_installation/kereta_database/install.sh
+++ docker_installation/kereta_database/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+echo "Installing Kereta Database..."
+
 KERETA_DATABASE_NAME="Kereta"
 KERETA_DATABASE_USER="root"
 KERETA_DATABASE_PASSWORD="root"
@@ -13,3 +15,5 @@ docker run --name mysql_$KERETA_DATABASE_NAME -e MYSQL_ROOT_PASSWORD=$KERETA_DAT
 
 #Connect through mysql client from another container
 #docker run -it --link my-container-name:mysql_$KERETA_DATABASE_NAME --rm mysql/mysql-server:tag sh -c 'exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$KERETA_DATABASE_PASSWORD"'
+
+echo "Done installing Kereta database..."
